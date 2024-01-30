7b05e92bbd799c3152d322e8a183c03077e0188c
Xlebabutti <110064356+Xlebabutti@users.noreply.github.com>
2024-01-30 13:48:59 +0300

    #fix jest

--- jest.config.ts
+++ jest.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
         // ...
         '^@/(.*)$': '<rootDir>/src/$1',
     },
+    modulePathIgnorePatterns: ['<rootDir>/tests/'],
     testEnvironment: 'jest-environment-jsdom',
 };
 
