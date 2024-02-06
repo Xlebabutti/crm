131339da64b305bb3d8d1ba9f0f69e7024bf74fc
Xlebabutti <110064356+Xlebabutti@users.noreply.github.com>
2024-02-06 20:09:17 +0300

    #fix: theme-provider

--- src/app/_providers/theme-provider.tsx
+++ src/app/_providers/theme-provider.tsx
@@ -1,8 +1,16 @@
 'use client';
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes/dist/types';
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-    return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+export function ThemeProvider({ children }: { children?: React.ReactNode }) {
+    return (
+        <NextThemesProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            {children}
+        </NextThemesProvider>
+    );
 }
