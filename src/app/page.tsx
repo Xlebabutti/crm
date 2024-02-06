00466208402bbb5fdd85ee7c587a5565a65d6d72
Xlebabutti <110064356+Xlebabutti@users.noreply.github.com>
2024-02-06 16:43:26 +0300

    #add tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/shared/ui/button';
+
 export default async function Home() {
-    return;
+    return (
+        <main className="flex min-h-screen flex-col p-8">
+            <h1 className="text-3xl mb-2">CRM</h1>
+            <Button></Button>
+        </main>
+    );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ['class'],
-    content: [
-        './pages/**/*.{ts,tsx}',
-        './components/**/*.{ts,tsx}',
-        './app/**/*.{ts,tsx}',
-        './src/**/*.{ts,tsx}',
-    ],
+    content: ['./src/**/*.{ts,tsx}'],
     prefix: '',
     theme: {
         container: {
@@ -17,6 +14,11 @@ module.exports = {
             },
         },
         extend: {
+            extend: {
+                fontFamily: {
+                    sans: ['var(--font-sans)', ...fontFamily.sans],
+                },
+            },
             colors: {
                 border: 'hsl(var(--border))',
                 input: 'hsl(var(--input))',
