6ae0535f91c45c50b3df66b2f6a12286307ef0af
Xlebabutti <vadimkozlovmedia@gmail.com>
2024-02-27 16:02:13 +0300

    #add logo

--- src/widgets/app-header/_ui/logo.tsx
+++ src/widgets/app-header/_ui/logo.tsx
@@ -5,7 +5,7 @@ export function Logo() {
     return (
         <Link className="flex items-center space-x-2" href="/">
             <LogoIcon className="h-6 w-6" />
-            <span className="font-bold inline-block">Micro courses</span>
+            {/* <span className="font-bold inline-block">GINJ</span> */}
         </Link>
     );
 }
